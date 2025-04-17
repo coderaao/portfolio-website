bde4ae93d6baf3172ebf8105ebddb4fbe025f7d8
coderaao <akbarovazizbek5560@gmail.com>
2025-04-17 19:41:27 +0500

    Fix import path for SpeedInsights component

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import { routeTree } from "./routeTree.gen";
 
